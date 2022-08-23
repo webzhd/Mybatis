9b14d4b560621f686090f5c88a40542c70d1d252
Duo Zhang <duo.zhang@raysightmed.com>
2022-08-23 12:22:46 +0800

    add mybatis

---
+++ Mybais-xml-crud/src/main/java/com/zd/Dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.zd.Dao;
+
+import com.zd.Pojo.User;
+
+public interface UserMapper {
+    User getUser(int id);
+}
---
+++ Mybais-xml-crud/src/main/java/com/zd/Utils/MybatisUtils.java
@@ -0,0 +1,27 @@
+package com.zd.Utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisUtils {
+    public static SqlSessionFactory sqlSessionFactory;
+    static {
+        String resource = "mybatis-config.xml";
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static SqlSession getSession() {
+        return sqlSessionFactory.openSession();
+    }
+}
